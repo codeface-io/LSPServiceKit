631c1d22edd631e0455e6178463c05c5895a1fc6
Sebastian Fichtner <contact@flowtoolz.com>
2022-05-08 21:59:32 +0100

    Bump minimum macOS version to 12 so we can use async / await APIs
    ... and drop promises

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.4.0
+// swift-tools-version:5.6.0
 
 import PackageDescription
 
 let package = Package(
     name: "LSPServiceKit",
-    platforms: [.iOS(.v11), .tvOS(.v11), .macOS(.v10_15)],
+    platforms: [.iOS(.v11), .tvOS(.v11), .macOS(.v12)],
     products: [
         .library(
             name: "LSPServiceKit",
@@ -14,19 +14,19 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/flowtoolz/SwiftLSP.git",
-            .branch("master")
+            branch: "master"
         ),
         .package(
             url: "https://github.com/flowtoolz/FoundationToolz.git",
-            .branch("master")
+            branch: "master"
         ),
         .package(
             url: "https://github.com/flowtoolz/SwiftObserver.git",
-            .branch("master")
+            branch: "master"
         ),
         .package(
             url: "https://github.com/flowtoolz/SwiftyToolz.git",
-            .branch("master")
+            branch: "master"
         ),
     ],
     targets: [

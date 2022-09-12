1c9121378ca0005e025511d5f9971334a7302509
Sebastian Fichtner <contact@flowtoolz.com>
2022-09-12 11:15:37 +0200

    Move types into LSP namespace for consistency

--- Code/ProjectLocation.swift
+++ Code/ProjectLocation.swift
@@ -1,15 +1,20 @@
 import Foundation
+import SwiftLSP
 
-public struct ProjectLocation: Codable, Equatable
-{
-    public init(folder: URL, language: String, codeFileEndings: [String])
-    {
-        self.folder = folder
-        self.language = language
-        self.codeFileEndings = codeFileEndings
-    }
+public extension LSP {
     
-    public var folder: URL
-    public let language: String
-    public let codeFileEndings: [String]
+    struct ProjectLocation: Codable, Equatable {
+        
+        public init(folder: URL,
+                    language: String,
+                    codeFileEndings: [String]) {
+            self.folder = folder
+            self.language = language
+            self.codeFileEndings = codeFileEndings
+        }
+        
+        public var folder: URL
+        public let language: String
+        public let codeFileEndings: [String]
+    }
 }

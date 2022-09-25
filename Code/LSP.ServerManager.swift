a9dea321d8dd078c258fd3df1805793bcb2394a4
Sebastian Fichtner <contact@flowtoolz.com>
2022-09-25 12:39:33 +0200

    Rework server manager. Adapt to changes in LSServiceKit and SwiftLSP.

--- Code/LSP.ServerManager.swift
+++ Code/LSP.ServerManager.swift
@@ -11,85 +11,59 @@ public extension LSP {
         
         private init() {}
         
-        public func getServer(for codebase: CodebaseLocation,
-                              handleConnectionError: @escaping (Error) -> Void = { log($0) }) async throws -> LSP.ServerCommunicationHandler {
+        public func getServer(for codebase: CodebaseLocation) async throws -> LSP.ServerCommunicationHandler {
             
-            if let activeCodebase = self.codebaseLocation,
-               activeCodebase == codebase,
-               let server = server,
-               let initialization {
-                // server has been created and initialization started for this codebase
-                try await initialization.assumeSuccess()
+            if let activeCodebase = codebaseLocation, activeCodebase == codebase,
+                let server, serverIsWorking {
                 return server
             }
             
-            // we have to recreate and initialize the server
-            self.codebaseLocation = codebase
+            reset()
             
-            let createdServer = try await createServer(language: codebase.language,
-                                                       handleConnectionError: handleConnectionError)
-            server = createdServer
+            // create new initialized server
+            let newServer = try await createServer(forLanguage: codebase.language)
+            let processID = try await LSPService.api.processID.get()
+            _ = try await newServer.request(.initialize(folder: codebase.folder,
+                                                        clientProcessID: processID))
+            try await newServer.notify(.initialized)
             
-            let createdInitialization = Self.initialize(createdServer, for: codebase)
-            initialization = createdInitialization
-            
+            // set result
+            codebaseLocation = codebase
+            server = newServer
             serverIsWorking = true
             
-            try await createdInitialization.assumeSuccess()
-            
-            return createdServer
+            return newServer
         }
         
-        private func createServer(language: String,
-                                  handleConnectionError: @escaping (Error) -> Void) async throws -> LSP.ServerCommunicationHandler {
+        private func createServer(forLanguage language: String) async throws -> LSP.ServerCommunicationHandler {
             
             let server = try LSPService.api.language(language.lowercased()).connectToLSPServer()
             
-            await server.setNotificationHandler { notification in
+            await server.handleNotificationFromServer { notification in
                 //            log("Server sent notification:\n\(notification.method)\n\(notification.params?.description ?? "nil params")")
             }
             
-            await server.setErrorOutputHandler { _ in
+            await server.handleErrorOutputFromServer { _ in
                 //            log("\(language.capitalized) language server sent message via stdErr:\n\($0)")
             }
             
-            await server.connection.didSendError = { error in
-                // TODO: do we (why don't we?) need to nil the server after the websocket sent an error, so that the server gets recreated and the websocket connection reinstated?? do we need to close the websocket connection?? ... maybe the LSPServerConnection protocol needs to expose more functions for handling the connection itself, like func closeConnection() and var connectionDidClose ...
-                
-                Task {
-                    await server.connection.close()
-                    self.serverIsWorking = false
-                    handleConnectionError(error)
-                }
-            }
-            
-            await server.connection.didClose = { [weak self] in
-                log(warning: "LSP websocket connection did close")
-                
-                self?.serverIsWorking = false
+            await server.handleConnectionShutdown { error in
+                log(error.readable)
+                self.reset()
             }
             
             return server
         }
         
-        private static func initialize(_ server: LSP.ServerCommunicationHandler,
-                                       for codebase: CodebaseLocation) -> Task<Void, Error> {
-            Task {
-                let processID = try await LSPService.api.processID.get()
-                
-                let _ = try await server.request(.initialize(folder: codebase.folder,
-                                                             clientProcessID: processID))
-                
-                //            try log(initializeResult: initializeResult)
-                
-                try await server.notify(.initialized)
-            }
+        private func reset()
+        {
+            serverIsWorking = false
+            codebaseLocation = nil
+            server = nil
         }
         
-        private var codebaseLocation: CodebaseLocation? = nil
-        private var initialization: Task<Void, Error>? = nil
-        private var server: LSP.ServerCommunicationHandler? = nil
-        
         @Published public var serverIsWorking = false
+        private var codebaseLocation: CodebaseLocation? = nil
+        private var server: LSP.ServerCommunicationHandler? = nil
     }
 }

50b8ff1fb6313c9a4e4e16d8b22f15b0d74df033
Sebastian Fichtner <contact@flowtoolz.com>
2023-03-05 15:35:15 +0100

    Bump dependency versions

--- Package.swift
+++ Package.swift
@@ -14,19 +14,19 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/codeface-io/SwiftLSP.git",
-            exact: "0.3.8"
+            exact: "0.3.9"
         ),
         .package(
             url: "https://github.com/flowtoolz/FoundationToolz.git",
-            exact: "0.1.7"
+            exact: "0.2.0"
         ),
         .package(
             url: "https://github.com/codeface-io/SwiftObserver.git",
-            exact: "7.0.6"
+            exact: "7.0.7"
         ),
         .package(
             url: "https://github.com/flowtoolz/SwiftyToolz.git",
-            exact: "0.3.0"
+            exact: "0.4.0"
         )
     ],
     targets: [

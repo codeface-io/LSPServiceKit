c00b7501c4ddb309d7903bf5bccd43e5c34ecaff
Sebastian Fichtner <contact@flowtoolz.com>
2023-03-28 19:27:47 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -14,19 +14,19 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/codeface-io/SwiftLSP.git",
-            exact: "0.3.9"
+            exact: "0.3.10"
         ),
         .package(
             url: "https://github.com/flowtoolz/FoundationToolz.git",
-            exact: "0.2.0"
+            exact: "0.3.0"
         ),
         .package(
             url: "https://github.com/codeface-io/SwiftObserver.git",
-            exact: "7.0.7"
+            exact: "7.0.8"
         ),
         .package(
             url: "https://github.com/flowtoolz/SwiftyToolz.git",
-            exact: "0.4.0"
+            exact: "0.5.0"
         )
     ],
     targets: [
